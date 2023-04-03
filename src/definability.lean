dba57431ebd51961db20d185933ee3e1feedd11e
airh4ck <dudnikoff98@gmail.com>
2023-04-03 17:55:48 +0300

    Implement proof of `subset_if_predicates_definable`

--- src/definability.lean
+++ src/definability.lean
@@ -4,7 +4,7 @@ import util
 
 namespace definability
 
-variable {L : lang}
+variables {L : lang}
 
 def vector.to_va {α : Type*} {n : ℕ+} (v : vector α n) [inhabited α] : ℕ → α :=
   λ var, if var < n then v.nth var else default 
@@ -26,30 +26,28 @@ def predicate_is_definable {L₁ L₂ : lang} (M₁ : struc L₁) {M₂ : struc 
       v ∈ p_int ↔ va ⊨ φ)
 
 /-- A set of all predicates definable in a structure `M` -/
-def definable_predicates (M₁ : struc L) : set (Σ (n : ℕ+), L.R n) :=
+def definable_predicates (M₁ : struc L) (M₂ : struc L) : set (Σ (n : ℕ+), L.R n) :=
     { p : Σ (n : ℕ+), L.R n | 
-      ∃ (M₂ : struc L) (p_int : M₂.R p.2) (univ_eq : M₁.univ = M₂.univ), 
+      ∃ (p_int : M₂.R p.2) (univ_eq : M₁.univ = M₂.univ), 
         predicate_is_definable M₁ univ_eq p_int }
 
-lemma mem_of_definable_predicates_iff_definable (M : struc L) {x : Σ (n : ℕ+), L.R n} :
-  x ∈ definable_predicates M ↔ ∃ (M' : struc L) (p_int : M'.R x.2) (univ_eq : M.univ = M'.univ),
-                               predicate_is_definable M univ_eq p_int :=
-begin
-  simp only [definable_predicates, set.mem_set_of]
-end
-
 open arithmetic_structure
+
 theorem subset_if_predicates_definable {L₁ : arith_lang} (S₁ S₂ : arith_struc L₁) :
   (∀ (p : Σ (n : ℕ+), L₁.to_lang.R n) 
-     (p_int : (arithmetic_structure.arith_struc.to_struc S₁).R p.2 ), 
+     (p_int : (arithmetic_structure.arith_struc.to_struc S₁).R p.2 ),
     predicate_is_definable S₂.to_struc (by simp [arith_struc.to_struc]) p_int)
-  → definable_predicates S₁.to_struc ⊆ definable_predicates S₂.to_struc :=
+  → ∃ (M₂ : struc L₁), definable_predicates ↑S₁ M₂ ⊆ definable_predicates ↑S₂ M₂ :=
 begin
-  intros h x x_property,
-  simp only [predicate_is_definable] at h,
-  
-  simp only [mem_of_definable_predicates_iff_definable, predicate_is_definable] at x_property ⊢,
-  sorry
+  intro h,
+  split,
+  { intros x h₁,
+    simp only [definable_predicates, set.mem_set_of] at h₁ ⊢,
+    rcases h₁ with ⟨p_int, univ_eq, h'⟩,
+    have s := h x p_int,
+    use p_int,
+    use univ_eq,
+    exact s }
 end
 
 end definability
